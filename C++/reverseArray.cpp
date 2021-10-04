8d54d1fe10f55905a028c6ca46184d057d0e8d94
kaushikboi <aritra.kaushik@gmail.com>
2021-10-04 18:05:06 +0530

    Create reverseArray.cpp

---
+++ C++/reverseArray.cpp
@@ -0,0 +1,38 @@
+//Given an array of integers, for example [1,8,9,10,23,2,4,13,17], write a program to reverse the array i.e. o/p should be [17,13,4,2,23,10,9,8,1]
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArray(int arr[], int start, int end)
+{
+	while (start < end)
+	{   int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+    }
+}	
+
+void printArray(int arr[], int size)
+{
+for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+	int n;
+    int arr[n];
+	
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+	printArray(arr, n);
+	reverseArray(arr, 0, n-1);
+	cout << "Reversed array is" << endl;
+	printArray(arr, n);
+	
+	return 0;
+}
