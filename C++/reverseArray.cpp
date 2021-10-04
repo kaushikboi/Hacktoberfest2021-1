6f41600a1ce64ce4e5e05910ff40d3e15ee30dbb
kaushikboi <aritra.kaushik@gmail.com>
2021-10-04 18:09:12 +0530

    Reverse an array

--- C++/reverseArray.cpp
+++ C++/reverseArray.cpp
@@ -1,8 +1,9 @@
-//Given an array of integers, for example [1,8,9,10,23,2,4,13,17], write a program to reverse the array i.e. o/p should be [17,13,4,2,23,10,9,8,1]
+//Problem statement : Given an array of integers, for example [1,8,9,10,23,2,4,13,17], write a program to reverse the array i.e. o/p should be [17,13,4,2,23,10,9,8,1]
+// Time Complexity : O(n) (Iterative method)
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverseArray(int arr[], int start, int end)
+void reverseArray(int arr[], int start, int end) //Function to reverse the array 
 {
 	while (start < end)
 	{   int temp = arr[start];
@@ -13,14 +14,14 @@ void reverseArray(int arr[], int start, int end)
     }
 }	
 
-void printArray(int arr[], int size)
+void printArray(int arr[], int size) //Function to print the array 
 {
 for (int i = 0; i < size; i++)
     cout << arr[i] << " ";
     cout << endl;
 }
 
-int main()
+int main() //Driver code
 {
 	int n;
     int arr[n];
